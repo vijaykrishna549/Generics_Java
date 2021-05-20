a3aa99667df54266212188758ef943f552990c6c
pvijaykrishna549 <you@example.com>
2021-05-20 06:46:13 +0530

    UC-2: Maximum Float between 3 Float Values

--- GenericDemoMax.java
+++ GenericDemoMax.java
@@ -13,5 +13,6 @@ public class GenericDemoMax {
 
     public static void main(String[] args) {
         System.out.println("Integer Max: " + max(Integer.valueOf(32), Integer.valueOf(56), Integer.valueOf(89)));
+        System.out.println("Double Max: " + max(Double.valueOf(5.6), Double.valueOf(7.8),Double.valueOf(10.25)));
     }
     }
