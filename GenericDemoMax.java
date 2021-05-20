d0f8518cbaa448154faeb362d4c5e5035c707f9d
pvijaykrishna549 <you@example.com>
2021-05-20 06:43:24 +0530

    UC-1: Maximum Integer from 3 Integers

---
+++ GenericDemoMax.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.generics;
+
+public class GenericDemoMax {
+    public static <T extends Comparable<T>> T max(T... elements) {
+        T max = elements[0];
+        for (T element : elements) {
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Integer Max: " + max(Integer.valueOf(32), Integer.valueOf(56), Integer.valueOf(89)));
+    }
+    }
